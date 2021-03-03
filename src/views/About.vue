2bd85d4400395cb1ae5d1da40eadf789344695d7
Tucker Austin <tucker.austin@gmail.com>
2021-03-03 11:59:04 -0800

    Initial commit
    Exploration into Vue composition API

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. In est iste quae
+      praesentium, dolores aperiam, error debitis repudiandae eius autem
+      nostrum, assumenda hic magnam rem enim excepturi. Minima blanditiis,
+      facere voluptate, quas odio enim est voluptatem, rem nulla sit eaque
+      delectus! Iure dignissimos ducimus culpa fugit alias iste illo quaerat.
+      Atque ab odit numquam. Aspernatur, adipisci fuga. Explicabo nihil illum
+      dolores ratione odio, modi distinctio labore sequi. Fugit sunt quam
+      molestias. Optio enim architecto, eos aliquid voluptates consequatur
+      asperiores sapiente et necessitatibus ad debitis, fugiat suscipit laborum
+      quaerat minima ipsam corporis doloremque quod voluptas minus neque
+      repudiandae. Harum, veniam qui.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus non eum
+      quo! Dolores, consequatur! Ea molestiae, aspernatur ipsa exercitationem
+      possimus repellendus quae amet deserunt cum ut maiores doloribus. Ea,
+      magni. Minima inventore reiciendis ipsa debitis culpa quidem, quibusdam
+      expedita maxime quasi ratione sint atque minus hic ex dolores blanditiis!
+      Ducimus facere tenetur libero sint. Cum voluptates laudantium consequatur
+      ab magni saepe reprehenderit earum, rerum ipsam? Vero harum laborum
+      accusamus perferendis magnam sed deleniti iure officia totam, eligendi
+      quia reiciendis ipsam consequuntur ex? Dolores odio molestias incidunt
+      sequi praesentium sint, veritatis porro qui quaerat inventore. Rerum
+      doloribus ipsam odit expedita in.
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <PostList v-if="showPosts" :posts="posts" />
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import PostList from "../components/PostList.vue";
+import { ref } from "vue";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      {
+        title: "welcome to the blog",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        id: 1,
+      },
+      { title: "top 5 CSS tips", body: "lorem ipsum", id: 2 },
+    ]);
+    const showPosts = ref(true);
+
+    return { posts, showPosts };
+  },
+};
 </script>
